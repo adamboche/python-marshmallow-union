76dede63c036ecb34a217b5583818b36537161e0
adamboche <adamboche@gmail.com>
2019-06-07 21:51:02 +0000

    Prepare package

---
+++ tests/test_schema.py
@@ -0,0 +1,35 @@
+import pytest
+import marshmallow
+
+
+class PersonSchema(marshmallow.Schema):
+    name = marshmallow.fields.String()
+    age = marshmallow.fields.String()
+
+
+class OtherSchema(marshmallow.Schema):
+    name = marshmallow.fields.String()
+    numbers = marshmallow.fields.List(marshmallow.fields.Integer())
+
+
+@pytest.mark.parametrize(
+    "data, schema,",
+    [
+        ({"name": "Alice", "age": "twenty-five"}, PersonSchema()),
+        ({"name": "Alice", "numbers": [25]}, OtherSchema()),
+    ],
+)
+def test_round_trip_schema(data, schema):
+
+    loaded = schema.load(data)
+    dumped = schema.dump(loaded)
+    assert dumped == loaded
+
+
+@pytest.mark.parametrize(
+    "data,schema", [({"name": "Alice", "numbers": 25}, OtherSchema())]
+)
+def test_raises(data, schema):
+    with pytest.raises(marshmallow.exceptions.ValidationError):
+        loaded = schema.load(data)
+        dumped = schema.dump(loaded)
