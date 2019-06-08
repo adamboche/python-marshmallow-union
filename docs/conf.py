214d0656bef2d2bcdecf62c0debe1b66b184d451
adamboche <adamboche@gmail.com>
2019-06-08 09:19:16 +0000

    Bump version: 0.1.9 → 0.1.10

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ project = "marshmallow-union"
 year = "2019"
 author = "Adam Boche"
 copyright = "{0}, {1}".format(year, author)
-version = release = "version = release = 0.1.9"
+version = release = "version = release = 0.1.10"
 
 pygments_style = "trac"
 templates_path = ["."]
