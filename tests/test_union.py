dd59aa675a917c37aacd402695a998510343649d
adamboche <adamboche@gmail.com>
2019-06-08 00:14:58 +0000

    Update tests for coverage

--- tests/test_union.py
+++ tests/test_union.py
@@ -6,8 +6,12 @@ import marshmallow_union
 
 class PersonSchema(marshmallow.Schema):
     name = marshmallow.fields.String()
-    age = marshmallow_union.Union(
-        [marshmallow.fields.String(), marshmallow.fields.Integer()]
+    number_or_numbers = marshmallow_union.Union(
+        [
+            marshmallow.fields.List(marshmallow.fields.Integer()),
+            marshmallow.fields.Integer(),
+        ],
+        reverse_serialize_candidates=True,
     )
 
 
@@ -17,22 +21,36 @@ class OtherSchema(marshmallow.Schema):
         [
             marshmallow.fields.List(marshmallow.fields.Integer()),
             marshmallow.fields.Integer(),
-        ]
+        ],
+        reverse_serialize_candidates=False,
     )
 
 
 @pytest.mark.parametrize(
     "data, schema",
     [
-        ({"name": "Alice", "age": "twenty-five"}, PersonSchema()),
-        ({"name": "Alice", "age": 25}, PersonSchema()),
-        ({"name": "Alice", "number_or_numbers": 25}, OtherSchema()),
+        ({"name": "Alice", "number_or_numbers": 25}, PersonSchema()),
+        ({"name": "Alice", "number_or_numbers": [25, 50]}, PersonSchema()),
         ({"name": "Alice", "number_or_numbers": [25, 50]}, OtherSchema()),
     ],
 )
 def test_round_trip(data, schema):
+    dumped = schema.dump(data)
+    loaded = schema.load(dumped)
+    assert dumped == loaded == data
 
-    loaded = schema.load(data)
-    dumped = schema.dump(loaded)
 
-    assert dumped == loaded
+@pytest.mark.parametrize(
+    "data,schema",
+    [
+        ({"name": "Alice", "number_or_numbers": "twenty-five"}, PersonSchema()),
+        ({"name": "Alice", "number_or_numbers": True}, PersonSchema()),
+        ({"name": "Alice", "number_or_numbers": {}}, PersonSchema()),
+    ],
+)
+def test_raises(data, schema):
+    with pytest.raises(marshmallow.exceptions.ValidationError):
+        dumped = schema.dump(data)
+
+    with pytest.raises(marshmallow.exceptions.ValidationError):
+        loaded = schema.load(data)
